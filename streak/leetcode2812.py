8286664ff0cb327d49916b2e47a207b55486e278
impulsivecliff7@gmail.com <impulsivecliff7@gmail.com>
2024-05-20 09:00:07 +0000

    completed leetcode2812

--- streak/leetcode2812.py
+++ streak/leetcode2812.py
@@ -1,4 +1,5 @@
 from collections import deque
+import heapq
 from typing import Deque, List
 
 
@@ -32,6 +33,27 @@ class Solution:
             
             return min_dist
         
+        min_dist = multisourceBFS()
+        max_heap = [(-min_dist[(0, 0)], 0, 0)]
+        visited = set()
+        visited.add((0, 0))
+
+        while max_heap:
+            dist, r, c = heapq.heappop(max_heap)
+            dist = -dist
+
+            if (r, c) == (N-1, N-1):
+                return dist
+            
+            neighbours = [[r + 1, c], [r - 1, c], [r, c + 1], [r, c - 1]]
+
+            for nei_r, nei_c in neighbours:
+                if in_bounds(nei_r, nei_c) and (nei_r, nei_c) not in visited:
+                    visited.add((nei_r, nei_c))
+                    
+                    heapq.heappush(max_heap, (-min(dist, min_dist[(nei_r, nei_c)]), nei_r, nei_c))
+            
+
 
 
                     
